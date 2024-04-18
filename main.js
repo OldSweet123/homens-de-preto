7d8256235f5f181f811a93a1da823bdbd9e986f6
Joao pedro <106817994+OldSweet123@users.noreply.github.com>
2024-04-17 22:43:52 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 
 for (let i=0;i < botoes.length; i++) {
-    botoes[i].onclick = function () {
+    botoes[i].onclick = function(){
 
         for (let j=0;j < botoes.length; j++){
             botoes[j].classList.remove("ativo");
@@ -14,3 +14,55 @@ for (let i=0;i < botoes.length; i++) {
         textos[i].classList.add("ativo");
     }
 }
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
+const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
+const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
+
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+contadores[1].textContent = calculaTempo(tempoObjetivo2);
+contadores[2].textContent = calculaTempo(tempoObjetivo3);
+contadores[3].textContent = calculaTempo(tempoObjetivo4);
+
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+
+
+
+
+function calculaTempo(tempoObjetivo) {
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+    if (tempoFinal > 0){
+        return [dias,horas,minutos,segundos]
+    } else {
+        return [0,0,0,0]
+    }
+    
+}
+
+function atualizarCronometro(){
+    document.getElementById("dias0").textContent = calculaTempo(tempos[0])[0];
+    document,getElementById("horas0").textContent = calculaTempo(tempos[0])[1];
+    document,getElementById("min0").textContent = calculaTempo(tempos[0])[2];
+    document,getElementById("seg0").textContent = calculaTempo(tempos[0])[3];
+    for (let i=0; i<contadores.length;i++){
+        contadores[i].textContent = calculaTempo(tempos[i]);
+    }
+}
+
+function comecaCronometro(){
+    atualizarCronometro();
+    setInterval(atualizarCronometro,1000)
+}
+
+comecaCronometro();
